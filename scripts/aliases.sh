26ddb4e4a44cfbd63aac61df548d3a7deb5a50d4
Abdomash <Abdomash2010@gmail.com>
2026-07-08 00:47:38 +0300

    feat: ibex alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -6,3 +6,4 @@ alias so='exec $SHELL -l'
 alias va="source .venv/bin/activate"
 alias vpn='sudo openconnect vpn.kaust.edu.sa --authgroup=Students'
 alias gr='cd "$(git rev-parse --show-toplevel)"'
+alias ibex='ssh alshaham@glogin.ibex.kaust.edu.sa'
