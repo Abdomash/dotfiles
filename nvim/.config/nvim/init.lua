65a34c0f4d56f5057c591d4128c4a01c8ab0b934
abdomash <abdomash2010@gmail.com>
2026-07-20 13:15:24 +0300

    refactor(nvim): organize and simplify config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
+-- Editor
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_node_provider = 0
@@ -30,9 +30,8 @@ vim.opt.confirm = true
 vim.opt.completeopt = { 'menuone', 'noselect', 'popup', 'fuzzy' }
 vim.opt.clipboard = 'unnamedplus'
 
-local function map(mode, lhs, rhs, opts)
-  opts = opts or {}
-  vim.keymap.set(mode, lhs, rhs, opts)
+local function map(lhs, rhs, desc)
+  vim.keymap.set('n', lhs, rhs, { desc = desc })
 end
 
 local function augroup(name)
@@ -46,6 +45,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+map('<esc>', '<cmd>nohlsearch<cr>', 'Clear search highlights')
+map('<C-h>', '<C-w><C-h>', 'Move to left split')
+map('<C-j>', '<C-w><C-j>', 'Move to lower split')
+map('<C-k>', '<C-w><C-k>', 'Move to upper split')
+map('<C-l>', '<C-w><C-l>', 'Move to right split')
+
+-- Packages
 local ts_languages = {
   'bash',
   'css',
@@ -63,117 +69,119 @@ local ts_languages = {
   'yaml',
 }
 
-vim.pack.add({
-  { src = 'https://github.com/Skardyy/makurai-nvim' },
-  { src = 'https://github.com/ibhagwan/fzf-lua' },
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-  { src = 'https://github.com/github/copilot.vim' },
-  { src = 'https://github.com/mason-org/mason.nvim' },
-  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-  { src = 'https://github.com/MunifTanjim/nui.nvim' },
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range('3') },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-  { src = 'https://github.com/echasnovski/mini.nvim' },
-})
-
-pcall(vim.cmd.colorscheme, 'makurai_dark')
-
 vim.api.nvim_create_autocmd('PackChanged', {
   group = augroup('minimal-pack-hooks'),
   callback = function(ev)
     local data = ev.data
-    if not data or data.spec.name ~= 'nvim-treesitter' then
+    if data.spec.name ~= 'nvim-treesitter' or (data.kind ~= 'install' and data.kind ~= 'update') then
       return
     end
-    if data.kind == 'install' or data.kind == 'update' then
-      vim.cmd.packadd('nvim-treesitter')
-      local ok, ts = pcall(require, 'nvim-treesitter')
-      if ok then
-        pcall(ts.install, ts_languages)
-      end
-    end
+
+    vim.cmd.packadd('nvim-treesitter')
+    require('nvim-treesitter').install(ts_languages)
   end,
 })
 
-local ts_ok = pcall(require, 'nvim-treesitter')
+vim.pack.add({
+  'https://github.com/Skardyy/makurai-nvim',
+  'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/github/copilot.vim',
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/mason-org/mason-lspconfig.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/nvim-lua/plenary.nvim',
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range('3') },
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/echasnovski/mini.nvim',
+}, { load = true })
 
+vim.cmd.colorscheme('makurai_dark')
+
+-- Treesitter
 vim.api.nvim_create_autocmd('FileType', {
   group = augroup('minimal-treesitter'),
   pattern = ts_languages,
-  callback = function(args)
-    pcall(vim.treesitter.start, args.buf)
-    if ts_ok then
-      vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end
+  callback = function(ev)
+    pcall(vim.treesitter.start, ev.buf)
+    vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
 
-local fzf_ok, fzf = pcall(require, 'fzf-lua')
-if fzf_ok then
-  fzf.setup({
-    winopts = {
-      preview = {
-        hidden = 'nohidden',
-      },
+-- Search and files
+local fzf = require('fzf-lua')
+fzf.setup({
+  winopts = {
+    preview = { hidden = 'nohidden' },
+  },
+  keymap = {
+    builtin = {
+      ['<C-d>'] = 'preview-page-down',
+      ['<C-u>'] = 'preview-page-up',
     },
-    keymap = {
-      builtin = {
-        ['<C-d>'] = 'preview-page-down',
-        ['<C-u>'] = 'preview-page-up',
-      },
-    },
-    files = {
-      fd_opts = table.concat({
-        '--color=never',
-        '--type=f',
-        '--hidden',
-        '--follow',
-        '--exclude=.git',
-      }, ' '),
-    },
-  })
-end
+  },
+  files = {
+    fd_opts = '--color=never --type=f --hidden --follow --exclude=.git',
+  },
+})
 
-local mini_icons_ok, mini_icons = pcall(require, 'mini.icons')
-if mini_icons_ok then
-  mini_icons.setup()
-  mini_icons.mock_nvim_web_devicons()
-end
+map('<leader>sf', fzf.files, 'Search files')
+map('<leader>sg', fzf.live_grep, 'Search by grep')
+map('<leader>sb', fzf.buffers, 'Search buffers')
+map('<leader>sh', fzf.help_tags, 'Search help')
+map('<leader>sd', fzf.diagnostics_document, 'Search diagnostics')
+map('<leader>sr', fzf.oldfiles, 'Search recent files')
 
-local neo_tree_ok, neo_tree = pcall(require, 'neo-tree')
-if neo_tree_ok then
-  neo_tree.setup({
-    filesystem = {
-      hijack_netrw_behavior = 'open_current',
-    },
-  })
-end
+local mini_icons = require('mini.icons')
+mini_icons.setup()
+mini_icons.mock_nvim_web_devicons()
 
-local gitsigns_ok, gitsigns = pcall(require, 'gitsigns')
-if gitsigns_ok then
-  gitsigns.setup({
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    preview_config = {
-      border = 'rounded',
-      style = 'minimal',
-      relative = 'cursor',
-    },
-  })
-end
+require('neo-tree').setup({
+  filesystem = {
+    hijack_netrw_behavior = 'open_current',
+  },
+})
 
-local mason_ok, mason = pcall(require, 'mason')
-if mason_ok then
-  mason.setup()
-end
+map('<leader>pv', '<cmd>Neotree current reveal<cr>', 'Open file explorer')
 
+-- Git
+local gitsigns = require('gitsigns')
+gitsigns.setup({
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  preview_config = {
+    border = 'rounded',
+    style = 'minimal',
+    relative = 'cursor',
+  },
+})
+
+map('[c', function()
+  if vim.wo.diff then
+    vim.cmd.normal({ '[c', bang = true })
+    return
+  end
+  gitsigns.nav_hunk('prev')
+end, 'Previous git hunk')
+
+map(']c', function()
+  if vim.wo.diff then
+    vim.cmd.normal({ ']c', bang = true })
+    return
+  end
+  gitsigns.nav_hunk('next')
+end, 'Next git hunk')
+
+map('<leader>hp', gitsigns.preview_hunk_inline, 'Preview git hunk inline')
+map('<leader>hd', gitsigns.diffthis, 'Diff current file')
+
+-- LSP
 local lsp_servers = {
   'lua_ls',
   'marksman',
@@ -185,17 +193,13 @@ local lsp_servers = {
   'yamlls',
 }
 
-local mason_lspconfig_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
-if mason_lspconfig_ok then
-  mason_lspconfig.setup({
-    ensure_installed = lsp_servers,
-  })
-end
+require('mason').setup()
+require('mason-lspconfig').setup({ ensure_installed = lsp_servers })
 
 for _, lhs in ipairs({ 'gra', 'gri', 'grn', 'grr', 'grt', 'grx', 'gO' }) do
-  pcall(vim.keymap.del, 'n', lhs)
+  vim.keymap.del('n', lhs)
 end
-pcall(vim.keymap.del, 'x', 'gra')
+vim.keymap.del('x', 'gra')
 
 vim.diagnostic.config({
   virtual_text = true,
@@ -204,55 +208,19 @@ vim.diagnostic.config({
   float = { border = 'rounded', source = 'if_many' },
 })
 
-map('n', '<esc>', '<cmd>nohlsearch<cr>')
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move to left split' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move to lower split' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move to upper split' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move to right split' })
-
-if neo_tree_ok then
-  map('n', '<leader>pv', '<cmd>Neotree current reveal<cr>', { desc = 'Open file explorer' })
-end
-
-if fzf_ok then
-  map('n', '<leader>sf', fzf.files, { desc = 'Search files' })
-  map('n', '<leader>sg', fzf.live_grep, { desc = 'Search by grep' })
-  map('n', '<leader>sb', fzf.buffers, { desc = 'Search buffers' })
-  map('n', '<leader>sh', fzf.help_tags, { desc = 'Search help' })
-  map('n', '<leader>sd', fzf.diagnostics_document, { desc = 'Search diagnostics' })
-  map('n', '<leader>sr', fzf.oldfiles, { desc = 'Search recent files' })
-end
-
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-map('n', '[d', function()
+map('<leader>q', vim.diagnostic.setloclist, 'Open diagnostics list')
+map('[d', function()
   vim.diagnostic.jump({ count = -1, float = true })
-end, { desc = 'Previous diagnostic' })
-map('n', ']d', function()
+end, 'Previous diagnostic')
+map(']d', function()
   vim.diagnostic.jump({ count = 1, float = true })
-end, { desc = 'Next diagnostic' })
-
-if gitsigns_ok then
-  map('n', '[c', function()
-    if vim.wo.diff then
-      vim.cmd.normal({ '[c', bang = true })
-      return
-    end
-    gitsigns.nav_hunk('prev')
-  end, { desc = 'Previous git hunk' })
-  map('n', ']c', function()
-    if vim.wo.diff then
-      vim.cmd.normal({ ']c', bang = true })
-      return
-    end
-    gitsigns.nav_hunk('next')
-  end, { desc = 'Next git hunk' })
-  map('n', '<leader>hp', gitsigns.preview_hunk_inline, { desc = 'Preview git hunk inline' })
-  map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff current file' })
-end
+end, 'Next diagnostic')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = augroup('minimal-lsp-attach'),
   callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+
     local function lsp_map(lhs, rhs, desc, mode)
       vim.keymap.set(mode or 'n', lhs, rhs, { buffer = ev.buf, desc = desc })
     end
@@ -260,25 +228,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
     lsp_map('K', vim.lsp.buf.hover, 'LSP hover')
     lsp_map('<leader>rn', vim.lsp.buf.rename, 'LSP rename')
     lsp_map('<leader>ca', vim.lsp.buf.code_action, 'LSP code action', { 'n', 'x' })
+    lsp_map('gd', fzf.lsp_definitions, 'LSP definitions')
+    lsp_map('gr', fzf.lsp_references, 'LSP references')
+    lsp_map('gi', fzf.lsp_implementations, 'LSP implementations')
+    lsp_map('gt', fzf.lsp_typedefs, 'LSP type definitions')
 
-    if fzf_ok then
-      lsp_map('gd', fzf.lsp_definitions, 'LSP definitions')
-      lsp_map('gr', fzf.lsp_references, 'LSP references')
-      lsp_map('gi', fzf.lsp_implementations, 'LSP implementations')
-      lsp_map('gt', fzf.lsp_typedefs, 'LSP type definitions')
-    else
-      lsp_map('gd', vim.lsp.buf.definition, 'LSP definitions')
-      lsp_map('gr', vim.lsp.buf.references, 'LSP references')
-      lsp_map('gi', vim.lsp.buf.implementation, 'LSP implementations')
-      lsp_map('gt', vim.lsp.buf.type_definition, 'LSP type definitions')
-    end
-
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion, ev.buf) then
+    if client:supports_method('textDocument/completion', ev.buf) then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
     end
 
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, ev.buf) then
+    if client:supports_method('textDocument/documentHighlight', ev.buf) then
       local group = augroup('minimal-lsp-highlight-' .. ev.buf)
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
         buffer = ev.buf,
@@ -288,9 +247,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'LspDetach' }, {
         buffer = ev.buf,
         group = group,
-        callback = function()
-          vim.lsp.buf.clear_references()
-        end,
+        callback = vim.lsp.buf.clear_references,
       })
     end
   end,
